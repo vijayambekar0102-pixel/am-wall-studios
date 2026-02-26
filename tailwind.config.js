165c8f2588baaa5b652f15e9c440d37f8751168d
vijayambekar0102-pixel <vijayambekar0102@gmail.com>
2026-02-26 20:27:34 +0530

    Create tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        amber: {
+          50: '#FFFBF0',
+          100: '#FEF3E2',
+          200: '#FDE68A',
+          300: '#FCD34D',
+          400: '#FBBF24',
+          500: '#F59E0B',
+          600: '#D97706',
+          700: '#B45309',
+          800: '#92400E',
+          900: '#78350F',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
